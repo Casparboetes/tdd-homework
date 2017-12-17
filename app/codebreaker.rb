d5351b35c89cc85e9ac1abac56992c2187ce4591
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2017-12-17 12:53:25 +0100

    Add guess method

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -11,17 +11,14 @@ class Codebreaker
       @secret_number = secret_number
 
       output.puts "Welcome to Codebreaker"
+
       output.puts "Enter guess:"
     end
 
     def guess(input)
-
-
-
-      # Make sure to replace next line with the actual implemented marking algorithm,
-      # using the @secret_number
-
-      #output.puts "you typed '#{input}'"
+      if input.length != 4
+        puts "Try guessing a number with four digits."
+      end
     end
   end
 end
